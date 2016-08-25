452bb48ba29f85757a00cfb7d20a4377318df7ac
Net <Net>
2016-08-25 13:10:05 -0400

    Maybe now

--- app/src/main/java/com/example/glowkitty/testing3/MainActivity.java
+++ app/src/main/java/com/example/glowkitty/testing3/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -33,41 +37,65 @@ public class MainActivity extends AppCompatActivity {
         test.notify(getBaseContext(), inputTxt.getText().toString(), 1);
     }
 
+    //non-initalized variables
     MediaPlayer mp;
-    ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-    public void turnPhoenixOnSwitch(View v) {
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+
+    //TODO: make this your own code and make it your own items on the navbar
+    private void addDrawerItems() { //add items to the navigation drawer
+        String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+    }
+
+
+    ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(); //all for a timer
+    public void turnPhoenixOnSwitch(View v) { //more timer
         try {
             executor.shutdownNow();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        executor = Executors.newSingleThreadScheduledExecutor();
-        Switch sw = (Switch) findViewById(R.id.switch1);
+        executor = Executors.newSingleThreadScheduledExecutor();//timer
+        Switch sw = (Switch) findViewById(R.id.switch1);//next 3 intitalize variables for the switch
         ImageView img = (ImageView) findViewById(R.id.phoenix);
         ImageView obj = (ImageView) findViewById(R.id.objection);
-        if (sw.isChecked()) {
-            img.setImageResource(R.drawable.pointer);
-            obj.setImageResource(R.drawable.objection);
+        if (sw.isChecked()) {//check if the switch is on or off
+            img.setImageResource(R.drawable.pointer);//set images
+            obj.setImageResource(R.drawable.objection);//^
 
-            obj.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake));
-            mp.start();
+            obj.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake));//shake dat ass
+            mp.start();//play music
 
-            Runnable task = new vanish(obj);
-
+            Runnable task = new vanish(obj);//this and next are timer and hide the OBJECTION!
             executor.schedule(task, 1000, TimeUnit.MILLISECONDS);
         } else {
-            img.setImageResource(R.drawable.sweaty);
+            img.setImageResource(R.drawable.sweaty);// turn everything off and back to normal
             obj.setImageResource(0);
         }
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Switch sw = (Switch) findViewById(R.id.switch1);
-        mp = MediaPlayer.create(this, R.raw.objection);
-        sw.setChecked(false);
+
+        Switch sw = (Switch) findViewById(R.id.switch1);//set the switch
+        sw.setChecked(false);//set the switch off justin case
+        mp = MediaPlayer.create(this, R.raw.objection);//initalize the mediaplayer to play the OBJECTION! sound
+
+        mDrawerList = (ListView)findViewById(R.id.navList);// start up the whole navigation drawer
+        addDrawerItems();
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
@@ -88,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
                 // Otherwise, set the URL to null.
                 Uri.parse("http://host/path"),
                 // TODO: Make sure this auto-generated app URL is correct.
+                // hint: its not
                 Uri.parse("android-app://com.example.glowkitty.testing3/http/host/path")
         );
         AppIndex.AppIndexApi.start(client, viewAction);
